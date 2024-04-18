980913ce7322fddd59dc9d79de8a9babf66cace6
hayeong120 <jbg01200249@gmail.com>
2024-04-18 19:21:07 +0900

    Test

---
+++ Kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+// 부생성자 - 매개변수를 통해 초기화
+class Bird2 {
+    var name:String
+    var wing:Int
+    var beak:String
+    var color:String
+
+    constructor(name:String, wing:Int, beak:String, color:String) {
+        this.name = name
+        this.wing = wing
+        this.beak = beak
+        this.color = color
+    }
+
+    fun fly() = println("Fly wing : $wing")
+    fun sing(vol:Int) = println("Sing vol : $vol")
+}
+fun main() {
+    val coco = Bird2("mybird",2,"short","blue")
+    coco.color = "yellow"
+    println("coco.color : ${coco.color}")
+    coco.fly()
+    coco.sing(3)
+}
